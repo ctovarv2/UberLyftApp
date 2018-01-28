305d0aa5dce86e3017939298378c629a1d737566
Juan Vasquez <kipvasq9@tamu.edu>
2018-01-28 04:14:28 -0600

    ui udpates

--- app/src/main/java/com/example/chris/tamuhack/MainActivity.java
+++ app/src/main/java/com/example/chris/tamuhack/MainActivity.java
@@ -1,51 +1,40 @@
 package com.example.chris.tamuhack;
 
 import android.Manifest;
-import android.content.pm.PackageManager;
-import android.support.design.widget.NavigationView;
-import android.support.v4.app.ActivityCompat;
-import android.support.v4.content.ContextCompat;
-import android.support.v7.app.AppCompatActivity;
 import android.content.Context;
+import android.content.pm.PackageManager;
 import android.location.Location;
 import android.location.LocationManager;
-
-import android.support.annotation.NonNull;
-import android.util.Log;
-import android.widget.HorizontalScrollView;
-import android.widget.TextView;
-import android.content.Intent;
 import android.os.Bundle;
+import android.preference.DialogPreference;
 import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.NavigationView;
 import android.support.design.widget.Snackbar;
-import android.view.View;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 import com.google.android.gms.common.api.Status;
-import com.google.android.gms.location.FusedLocationProviderClient;
-import com.google.android.gms.location.LocationServices;
-import com.google.android.gms.location.places.ui.PlaceAutocompleteFragment;
 import com.google.android.gms.location.places.Place;
+import com.google.android.gms.location.places.ui.PlaceAutocompleteFragment;
 import com.google.android.gms.location.places.ui.PlaceSelectionListener;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.android.gms.tasks.Task;
-
-import org.json.JSONArray;
-import org.json.JSONObject;
-
-import java.util.List;
-
 import com.google.android.gms.maps.model.MarkerOptions;
+
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener,
@@ -76,19 +65,27 @@ public class MainActivity extends AppCompatActivity
         setSupportActionBar(toolbar);
 
         // Action Button (Bottom Left Corner)
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        final FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 if(showing == true){
-                    findViewById(R.id.selections).setVisibility(view.INVISIBLE);
-                    /*Snackbar.make(view, "Minimized Rides.", Snackbar.LENGTH_LONG)
-                            .setAction("Action", null).show();*/
+                    findViewById(R.id.uber_selections).setVisibility(view.INVISIBLE);
+                    Snackbar.make(view, "Minimized Rides.", Snackbar.LENGTH_LONG)
+                            .setAction("Action", null).show();
+                    findViewById(R.id.lyft_selections).setVisibility(view.INVISIBLE);
+                    Snackbar.make(view, "Minimized Rides.", Snackbar.LENGTH_LONG)
+                            .setAction("Action", null).show();
+                    fab.setImageResource(android.R.drawable.arrow_up_float);
                     showing = false;
                 } else {
-                    findViewById(R.id.selections).setVisibility(view.VISIBLE);
-                    Snackbar.make(view, "Confirm Ride Selection.", Snackbar.LENGTH_LONG)
-                            .setAction("Action", null).show();
+                    findViewById(R.id.uber_selections).setVisibility(view.VISIBLE);
+                    /*Snackbar.make(view, "Confirm Ride Selection.", Snackbar.LENGTH_LONG)
+                            .setAction("Action", null).show();*/
+                    findViewById(R.id.lyft_selections).setVisibility(view.VISIBLE);
+                    /*Snackbar.make(view, "Confirm Ride Selection.", Snackbar.LENGTH_LONG)
+                            .setAction("Action", null).show();*/
+                    fab.setImageResource(android.R.drawable.arrow_down_float);
                     showing = true;
                 }
             }
@@ -171,22 +168,15 @@ public class MainActivity extends AppCompatActivity
                 LatLng midpoint = new LatLng((destinationCoords.latitude + userCoords.latitude) / 2.0,
                         (destinationCoords.longitude + userCoords.longitude) / 2.0);
 
-                //System.out.println("Dest: " + destinationCoords + "\n, User:" + userCoords + "\n, Midpoint: " + midpoint);
-
-                // move camera to midpoint with default zoom
-                // zoom = 7;
                 gMap.moveCamera(CameraUpdateFactory.newLatLngZoom(midpoint, zoom));
 
                 // if necessary adjust zoom to encompass the two markers
-
                 /*while(!gMap.getProjection().getVisibleRegion().latLngBounds.contains(userCoords) ||
                         !gMap.getProjection().getVisibleRegion().latLngBounds.contains(destinationCoords)){
 
                     System.out.println("NOT INCLUDED.");
                     gMap.moveCamera(CameraUpdateFactory.newLatLngZoom(midpoint, zoom - 1));
                 }*/
-
-                System.out.println("ZOOM: " + zoom);
             }
 
             @Override
@@ -208,6 +198,7 @@ public class MainActivity extends AppCompatActivity
                     1);
         }
 
+        // Once We Have Location Services Enabled By Default, Get Location
         LocationManager locManager = (LocationManager)getSystemService(Context.LOCATION_SERVICE);
         Boolean network_enabled = locManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
         Location location;
@@ -249,22 +240,13 @@ public class MainActivity extends AppCompatActivity
     public void onMapReady(GoogleMap googleMap){
         gMap = googleMap;
 
-        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(userCoords, zoom));
+        //gMap.setTrafficEnabled(true);
 
-        googleMap.addMarker(new MarkerOptions().position(userCoords)
+        gMap.moveCamera(CameraUpdateFactory.newLatLngZoom(userCoords, zoom));
+
+        gMap.addMarker(new MarkerOptions().position(userCoords)
             .title(userCoords + "Your Location."));
     }
-
-//    public void onMapReady(GoogleMap map){
-//        mMap = map;
-//
-//        onComplete();
-//    }
-
-//    public void onComplete(){
-//        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(
-//                new LatLng(userLatitude, userLongitude), 15));
-//    }
 
     @Override
     public void onBackPressed() {
@@ -292,6 +274,7 @@ public class MainActivity extends AppCompatActivity
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
+            System.out.println("hi");
             return true;
         }
 
@@ -304,38 +287,121 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        if (id == R.id.nav_about) {
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setMessage("TAMUHack 2018. \n\nTeam Members: \n\t\t\t\tTyler Carlson \n\t\t\t\tChristian Tovar \n\t\t\t\tJuan Vasquez")
+                    .setTitle("About")
+                    .setPositiveButton("Okay", new DialogInterface.OnClickListener(){
+                        public void onClick(DialogInterface dialog, int id){
+                            // handle the clicking
+                        }
+                    });
+            builder.create();
+            builder.show();
+        } else if (id == R.id.nav_settings) {
+            //            MAP_TYPE_NORMAL: Basic map.
+            //            MAP_TYPE_SATELLITE: Satellite imagery.
+            //            MAP_TYPE_HYBRID: Satellite imagery with roads and labels.
+            //            MAP_TYPE_TERRAIN: Topographic data.
+            //            MAP_TYPE_NONE: No base map tiles.
 
-        } else if (id == R.id.nav_slideshow) {
+            final CharSequence[] modes = {
+                    "Normal", "Satellite", "Hybrid", "Topological", "None"
+            };
 
-        } else if (id == R.id.nav_manage) {
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setTitle("Map Style")
+                    .setNegativeButton("Cancel", new DialogInterface.OnClickListener(){
+                        public void onClick(DialogInterface dialog, int id){
+                            // handle the clicking
+                        }
+                    })
+                    .setItems(modes, new DialogInterface.OnClickListener(){
+                        public void onClick(DialogInterface dialog, int which) {
+                            switch (which) {
+                                case 0:
+                                    gMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+                                    break;
+                                case 1:
+                                    gMap.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
+                                    break;
+                                case 2:
+                                    gMap.setMapType(GoogleMap.MAP_TYPE_HYBRID);
+                                    break;
+                                case 3:
+                                    gMap.setMapType(GoogleMap.MAP_TYPE_TERRAIN);
+                                    break;
+                                case 4:
+                                    gMap.setMapType(GoogleMap.MAP_TYPE_NONE);
+                                    break;
+                                default:
+                                    gMap.setMapType(GoogleMap.MAP_TYPE_NONE);
+                                    break;
+                            }
+                        }
+                    });
 
-        } else if (id == R.id.nav_share) {
+            builder.create();
+            builder.show();
+        } else if (id == R.id.nav_traffic) {
+            final CharSequence[] traffic = {
+                    "Traffic"
+            };
 
+            final ArrayList seletedItems = new ArrayList();
+
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setTitle("Enable Traffic Data")
+                    .setPositiveButton("Okay", new DialogInterface.OnClickListener(){
+                        public void onClick(DialogInterface dialog, int id){
+                            // handle the clicking
+                        }
+                    })
+                    .setMultiChoiceItems(traffic, null, new DialogInterface.OnMultiChoiceClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int indexSelected, boolean isChecked) {
+                            if (isChecked) {
+                                // If the user checked the item, add it to the selected items
+                                seletedItems.add(indexSelected);
+                                gMap.setTrafficEnabled(true);
+                            } else if (seletedItems.contains(indexSelected)) {
+                                // Else, if the item is already in the array, remove it
+                                seletedItems.remove(Integer.valueOf(indexSelected));
+                                gMap.setTrafficEnabled(false);
+                            }
+                        }
+                    });
+            builder.create();
+            builder.show();
+        } /*else if (id == R.id.nav_manage) {
+
+        }*/ else if (id == R.id.nav_share) {
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setMessage("Share your insightful information with us (kipvasq9@tamu.edu) or to " +
+                    "your friends!")
+                    .setTitle("Share")
+                    .setPositiveButton("Okay", new DialogInterface.OnClickListener(){
+                        public void onClick(DialogInterface dialog, int id){
+                            // handle the clicking
+                        }
+                    });
+            builder.create();
+            builder.show();
         } else if (id == R.id.nav_send) {
-
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setMessage("Share your harsh experiences with us! (kipvasq9@tamu.edu)")
+                    .setTitle("Report Problem")
+                    .setPositiveButton("Okay", new DialogInterface.OnClickListener(){
+                        public void onClick(DialogInterface dialog, int id){
+                            // handle the clicking
+                        }
+                    });
+            builder.create();
+            builder.show();
         }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
-    }
-
-    public void saveAddress(View view){
-
-
-//        TextView tv1 = (TextView)findViewById(R.id.textView3);
-//        EditText inputField = (EditText) findViewById(R.id.editText);
-//        String address = inputField.getText().toString();
-//
-//        INPUT_ADDRESS = address;
-//
-//        tv1.setText(INPUT_ADDRESS);
-
-        //Fragment fragmentById = getFragmentManager().findFragmentById(R.id.place_autocomplet‌​e_fragment);
-
-
     }
 }
